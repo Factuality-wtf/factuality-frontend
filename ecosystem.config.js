2fb09d7b954ccd99ed13cff77c1a3f6ac11fe4bd
t0nylombardi <alombardi.331@gmail.com>
2025-04-20 00:11:22 -0400

    chore: update deployment script to use PM2 for service restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
     args: 'start',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 3000,
+      NEXT_PUBLIC_API_URL: "https://factuality.wtf"
     }
   }]
 };
