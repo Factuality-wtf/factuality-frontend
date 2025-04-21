ce6f019b0f0beb2fe0c386b6b05e3cf62a5b5795
t0nylombardi <alombardi.331@gmail.com>
2025-04-21 17:37:24 -0400

    WIP: update PNPM setup and refine ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'frontend',
-    script: 'pnpm',
-    args: 'start',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      NEXT_PUBLIC_API_URL: "https://factuality.wtf"
-    }
-  }]
+  apps: [
+    {
+      name: "factuality-frontend",
+      cwd: "/home/deployer/actions-runner/_work/factuality-frontend/factuality-frontend",
+      script: "pnpm",
+      args: "start",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
 };
