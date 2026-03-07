2938eeda4f847949502cb72abfcfedbf3c522c19
Anthony Lombardi <alombardi.331@gmail.com>
2026-03-07 16:30:12 -0500

    feat: Add DEFAULT_FACT constant for initial fact display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import FactSection from "@components/FactSection";
+import { DEFAULT_FACT } from "@/lib/factDefaults";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center md:items-start justify-between text-text w-3/4 m-4">
       <div className="flex flex-col text-center md:text-left items-start justify-between gap-y-4">
-        <FactSection initialFact="Discover random facts about anything and everything all in one place!" />
+        <FactSection initialFact={DEFAULT_FACT} />
       </div>
     </main>
   );
