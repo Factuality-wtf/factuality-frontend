6b5c54f6db510ec577379e7d5de91dd655fe253d
Anthony Lombardi <alombardi.331@gmail.com>
2026-03-09 17:46:11 -0400

    feat: Enhance metadata generation and refactor FactPage

--- src/app/facts/[id]/[slug]/page.tsx
+++ src/app/facts/[id]/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { cache } from "react";
+import type { Metadata } from "next";
 import FactSection from "@/components/FactSection";
 import { FactClient } from "@/lib/factClient";
 
@@ -8,11 +10,39 @@ type PageProps = {
   }>;
 };
 
+const client = new FactClient();
+
+export const getFact = cache(async (id: string) => {
+  return client.getFactById(id);
+});
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { id, slug } = await params;
+
+  const fact = await getFact(id);
+
+  return {
+    title: "Did You Know?",
+    description: fact.body,
+    openGraph: {
+      title: "Did You Know?",
+      description: fact.body,
+      url: `/${id}/${slug}`,
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      description: fact.name,
+    },
+  };
+}
+
 export default async function FactPage({ params }: PageProps) {
   const { id } = await params;
 
-  const client = new FactClient();
-  const fact = await client.getFactById(id);
+  const fact = await getFact(id);
 
   return <FactSection initialFact={fact} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import FactSection from "@components/FactSection";
 import { DEFAULT_FACT } from "@/lib/factDefaults";
 
